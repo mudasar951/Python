a4ff2e8ea901457c6d936e2bcfd820277e87a235
mudasar951 <120493860+mudasar951@users.noreply.github.com>
2025-05-08 02:37:34 +0500

    Create 3_1_names.py

---
+++ Eric_Matthes_Python_Crash_Course_A_Hands/chap3/3_1_names.py
@@ -0,0 +1,8 @@
+names = ['Ali', 'Zain', 'Solomon']
+print(names[0].title())
+print(names[1].title())
+print(names[2].title())
+
+print(names[-1].title())
+print(names[-2].title())
+print(names[-3].title())
