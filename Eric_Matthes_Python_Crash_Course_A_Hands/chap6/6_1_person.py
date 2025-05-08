509b4796fef8b452af0a2892f81248ff7567cd60
mudasar951 <120493860+mudasar951@users.noreply.github.com>
2025-05-09 02:33:04 +0500

    Create 6_1_person.py

---
+++ Eric_Matthes_Python_Crash_Course_A_Hands/chap6/6_1_person.py
@@ -0,0 +1,8 @@
+# 6-1. Person: Use a dictionary to store information about a person you know. 
+#Store their first name, last name, age, and the city in which they live. You 
+#should have keys such as first_name, last_name, age, and city. Print each 
+#piece of information stored in your dictionary.
+
+person = {'first_name':'Hannan', 'last_name':'Rasool', 'age':19, 'city':'Abu dabi'}
+
+print(f"first_name = {person['first_name']}\nlast_name = {person['last_name']}\nage = {person['age']}\ncity = {person['city']}")
