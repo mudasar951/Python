b23ccbc1dcab10742bb3a8ca73fe4fe08d0b03f8
mudasar951 <120493860+mudasar951@users.noreply.github.com>
2025-05-16 23:00:28 +0500

    Create 9_1_restaurant.py

---
+++ Eric_Matthes_Python_Crash_Course_A_Hands/chap9/9_1_restaurant.py
@@ -0,0 +1,28 @@
+#  9-1. Restaurant: Make a class called Restaurant. The __init__() method for 
+# Restaurant should store two attributes: a restaurant_name and a cuisine_type. 
+# Make a method called describe_restaurant() that prints these two pieces of 
+# information, and a method called open_restaurant() that prints a message indi
+# cating that the restaurant is open.
+#  Make an instance called restaurant from your class. Print the two attri
+# butes individually, and then call both methods.
+
+class Restaurant:
+    def __init__(self,restaurant_name,cuisine_type):
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+
+    def describe_restaurant(self):
+        print(f"restaurant_name = {self.restaurant_name}, cuisine_type = {self.cuisine_type}")
+
+
+    def open_restaurant(self):
+        print(f"{self.restaurant_name} is open")
+
+
+restaurant = Restaurant('abc','xyz')
+
+print(restaurant.restaurant_name)
+print(restaurant.cuisine_type)
+
+restaurant.describe_restaurant()
+restaurant.open_restaurant()
